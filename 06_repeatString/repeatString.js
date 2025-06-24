00e066df5301189ae1e0ff63b2d7785842839336
Sham Safarany <125689014+Shamsafarany@users.noreply.github.com>
2025-06-24 15:00:48 +0300

    finish 15

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, x) {
+    let result = "";
+    for (let i = 0; i < x; i++) {
+        result.concat(string);
+    }
+    return result;
 };
 
 // Do not edit below this line
