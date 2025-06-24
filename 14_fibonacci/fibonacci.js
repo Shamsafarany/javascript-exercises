cc64864d947b246be231c1fe9212a3e6b255ebee
Sham Safarany <125689014+Shamsafarany@users.noreply.github.com>
2025-06-24 14:39:41 +0300

    finish 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return 'OOPS';
+    } else {
+    if (typeof(num) === "string") {
+        num = Number(num);
+    }    
+    if (num === 0 || num === 1) {
+    return num;
+    } else {
+        return (fibonacci(num - 1) + fibonacci(num - 2));
+    }
+ }
 
+    
 };
 
 // Do not edit below this line
